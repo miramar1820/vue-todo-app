343df323707ff648608f559e12da5501b9b41086
miramar1820 <miramar1820@gmail.com>
2023-09-06 08:04:39 +0600

    Home page styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,76 @@
 
 <template>
   <section class="hero is-medium">
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
     <div class="hero-body">
       <div class="container">
-        <p class="title is-info">
-          Hello, freind
-        </p>
-        <p class="subtitle">
-          My name is Miras, and this is a my pet project on Vue 3
-        </p>
+        <div class="content">
+          <p class="title is-info is-size-1-desktop is-size-3-mobile">
+            Hello
+          </p>
+          <p class="subtitle is-size-3-desktop">
+            My name is Miras, I am frontend developer,  and this is a my project on Vue3
+          </p>
+
+        </div>
       </div>
     </div>
 
 
   </section>
 </template>
+
+<style lang="scss" scoped>
+
+.hero {
+  flex: 1;
+}
+.bg {
+  animation: slide 3s ease-in-out infinite alternate;
+  background-image: linear-gradient(-60deg, #6c3 50%, #09f 50%);
+  bottom: 0;
+  left: -50%;
+  opacity: .5;
+  position: fixed;
+  right: -50%;
+  top: 0;
+  z-index: -1;
+}
+
+.bg2 {
+  animation-direction: alternate-reverse;
+  animation-duration: 4s;
+}
+
+.bg3 {
+  animation-duration: 5s;
+}
+
+.content {
+  background-color: rgba(255, 255, 255, .8);
+  border-radius: .25em;
+  box-shadow: 0 0 .25em rgba(0, 0, 0, .25);
+  // left: 50%;
+  padding: 10vmin;
+  // position: fixed;
+  text-align: center;
+  // top: 50%;
+  // transform: translate(-50%, -50%);
+}
+
+h1 {
+  font-family: monospace;
+}
+
+@keyframes slide {
+  0% {
+    transform: translateX(-25%);
+  }
+
+  100% {
+    transform: translateX(25%);
+  }
+}
+</style>
